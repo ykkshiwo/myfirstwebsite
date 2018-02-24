495207f4adcacd984ef70761331da2fc13c0367f
yangkangkai <452306233@qq.com>
2018-02-24 10:01:04 +0800

    20180224_10:00

---
+++ article/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse, request
+# Create your views here.
+def index(request):
+    return HttpResponse("hello, django")
--- myfirstwebsite/settings.py
+++ myfirstwebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mv(ak331sk*%&o1_kof%8pt(dxi@-^-rmzv%ejp%&s70g_we@0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.kkykykk.club', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
 ]
 
 MIDDLEWARE = [
